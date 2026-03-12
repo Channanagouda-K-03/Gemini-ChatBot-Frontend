79bc35466de2f71ddf9bd97a2af96180ab64e516
Channanagouda K <kchannanagouda4@gmail.com>
2026-03-12 18:21:47 +0530

    Initial commit of frontend

---
+++ src/services/api.jsx
@@ -0,0 +1,14 @@
+
+import axios from "axios";
+
+const API_URL = "https://gemini-chatbot-application-2.onrender.com";
+
+export const fetchChatResponse = async (question) => {
+    try {
+        const response = await axios.post(`${API_URL}/api/qna/ask`, { question });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching chat response:", error);
+        throw error;
+    }
+}
