675f68c7c95cfe9d28294f59c9fe4eaa4bcd0e2a
Channanagowda K <159411835+Channanagouda-K-03@users.noreply.github.com>
2026-03-12 20:31:20 +0530

    Update API_URL and format axios request

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,14 +1,16 @@
-
 import axios from "axios";
 
-const API_URL = "https://gemini-chatbot-application-2.onrender.com/api/qna/ask";
+const API_URL = "https://gemini-chatbot-application-2.onrender.com";
 
 export const fetchChatResponse = async (question) => {
     try {
-        const response = await axios.post(`${API_URL}/api/qna/ask`, { question });
+        const response = await axios.post(
+            `${API_URL}/api/qna/ask`,
+            { question }
+        );
         return response.data;
     } catch (error) {
         console.error("Error fetching chat response:", error);
         throw error;
     }
-}
+};
